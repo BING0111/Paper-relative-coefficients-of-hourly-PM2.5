b722a3c461cc50671111e0c02cfda2610a2c3b79
BING0111 <guo_xiaobing@hotmail.com>
2018-07-11 16:26:58 +0800

    a little problematic change

--- simulation.R
+++ simulation.R
@@ -155,3 +155,97 @@ ggplot(data = psts) +
   scale_y_continuous(name = '') + 
   scale_x_continuous(name = 'gamma', limits = c(-0.01, 0.51), breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5)) + 
   theme_bw()
+# Simulation of the full workflow with completed dataset ------------------
+
+# Construct the completed dataset
+load(".../Project - DATA/paper 2nd data/dat_paper2.RData")
+dat <- dat.paper2 %>% 
+  filter(City == 5101) %>% 
+  select(Date, str_subset(names(.), 'Hour'))
+
+nday <- nrow(dat)
+ncol <- 3
+hmean <- apply(dat[,-1], 2, mean, na.rm = TRUE)
+for (i in 1:ncol) {
+  dat[, i+1] <- replace(dat[, i+1], is.na(dat[, i+1]), hmean[i])
+}
+
+gamma   <- c(0.3, 0.2, 0.5)
+# gamma   <- c(rep(0.01, 6), rep(0.06, 2), rep(0.08, 4), rep(0.047, 10), rep(0.015,2))
+daymean <- apply(dat[,2:(ncol+1)], 1, function(x) sum(x*gamma))
+b.true  <- 0.005
+want.true <- b.true * gamma
+
+f1 <- b.true * daymean
+mu <- exp(f1)
+y  <- rpois(nday, mu) 
+Hour <- gather(dat[,1:(ncol+1)], key = h, value = x, -Date) %>% arrange(Date)
+
+dat.cent <- apply(dat[,2:(ncol+1)], 2, function(x) x - mean(x))
+dat.cent <- as.data.frame(cbind(Date = 1:nday, dat.cent))
+Hour.cent <- gather(dat.cent, key = h, value = x, -Date) %>% arrange(Date)
+
+# 2. Remove some points to get the missing dataset
+# dat.miss <- dat[,-2]
+# missrate <- 0.1
+# na.index <- vector('list', 24)
+# for (i in 1:24) {
+#   na.index[[i]]   <- sort(sample(1:365, missrate*365, replace = FALSE))
+#   dat.miss[, i+1] <- replace(dat.miss[, i+1], na.index[[i]], NA)
+# }
+# 
+# Hour <- gather(dat.miss, key = h, value = x, -Date) %>% arrange(Date)
+
+# 3. Imputation - random walk
+sigma.x <- sd(-diff(Hour$x), na.rm = TRUE)  
+nhour <- nrow(Hour)
+hmat <- matrix(1:(nday*ncol), nrow = nday, ncol = ncol, byrow = TRUE)
+
+dat.use <- list(y = y, x1 = Hour$x, sigma.x1 = sigma.x, hmat = hmat, nhour = nhour, nday  = nday, ncol = ncol) 
+inits   <- list(want = rep(0.005*0.33, 3))
+params  <- c('want')
+
+# jagsUI package
+time1 <- Sys.time()
+out <- jagsUI::jags(data = dat.use, inits = NULL, parameters.to.save = params,
+                    model.file = "similation_model_3_2.txt",
+                    n.chains = 3, n.adapt = 1000, n.iter = 20000, n.burnin = 5000, n.thin = 5,
+                    parallel = TRUE, n.cores = 3, verbose = T)
+time2 <- Sys.time()
+time3 <- Sys.time()
+out <- update(out, n.iter = 10000)
+time4 <- Sys.time()
+
+summary(out)
+out$Rhat
+out$summary
+out$samples # This is a mcmc.list object.
+
+
+# 4. Imputation - measurement error
+getmu <- function(v) {
+  n <- length(v)
+  v1 <- v2 <- v
+  for (i in 2:n) { v1[i] <- ifelse(is.na(v1[i]), v1[i-1], v1[i]) }
+  for (i in (n-1):1) { v2[i] <- ifelse(is.na(v2[i]), v2[i+1], v2[i]) }
+  vnew <- apply(data.frame(v1, v2), 1, mean, na.rm=T)
+  return(vnew)
+}
+sigma.x <- sd(-diff(Hour$x), na.rm = TRUE) 
+mu.x1 <- getmu(Hour$x)
+
+dat.use <- list(y = y, x1 = Hour$x, hmat= hmat, nhour = nhour, nday = nday, sigma.x1 = sigma.x, mu.x1 = mu.x1)
+initial <- NULL
+params  <- c('beta1', 'gamma')
+
+# jagsUI package
+out <- jags(data = dat.use, inits = NULL, parameters.to.save = params,
+            model.file = "similation_model_4.txt",
+            n.chains = 3, n.adapt = 2000, n.iter = 20000, n.burnin = 10000, n.thin = 10,
+            parallel = TRUE, n.cores = 3)
+# out <- update(out, n.iter = 1000)
+
+summary(out)
+out$Rhat
+out$summary
+out$samples # This is a mcmc.list object.
